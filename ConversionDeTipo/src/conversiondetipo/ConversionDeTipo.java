61ad323b4c0db5677c584315287119a431a8c273
NachoLorente <ilc1990@hotmail.com>
2022-09-27 10:27:02 +0200

    Introducir texto
    Hola

--- ConversionDeTipo/src/conversiondetipo/ConversionDeTipo.java
+++ ConversionDeTipo/src/conversiondetipo/ConversionDeTipo.java
@@ -15,7 +15,7 @@ public class ConversionDeTipo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }

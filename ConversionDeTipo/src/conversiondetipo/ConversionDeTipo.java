e00c1dc9e9026e6f8ec1ce36307d9758c52c82d5
NachoLorente <ilc1990@hotmail.com>
2022-09-28 09:18:45 +0200

    Modificación
    Desarrollado el programa completo

--- ConversionDeTipo/src/conversiondetipo/ConversionDeTipo.java
+++ ConversionDeTipo/src/conversiondetipo/ConversionDeTipo.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conversiondetipo;
 
-/**
- *
- * @author Alumno Mañana
- */
+import java.util.Scanner;
+
 public class ConversionDeTipo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        //Instancia una variable de la clase Scanner
+        Scanner lectura = new Scanner(System.in);
+        //Solicitamos por teclado el nº
+        System.out.println("introduce el nº entero que quieres que muestre: ");
+        //Convierto el String a tipo entero
+        int entero = Integer.parseInt(lectura.nextLine());
+        //Muestro el resultado por pantalla
+        System.out.println("El entero que has teclado es: " +entero);
     }
     
 }
